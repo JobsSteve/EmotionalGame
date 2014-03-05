cbfdc95cf4be9c090172926f57c801c69f8f8d23
artoorix <art.igberdin@gmail.com>
2014-03-05 16:53:18 +0400

    Initial Commit

---
+++ ThreeButtonProject/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  ThreeButtonProject
+//
+//  Created by artoorix on 05.03.14.
+//  Copyright Артур 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ ThreeButtonProject/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ThreeButtonProject
+//
+//  Created by artoorix on 05.03.14.
+//  Copyright Артур 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
